e283c6bf3806bb3b5cb4e54531cb1d2a7a75b8aa
Tabmex2023 <tobiasstuevel@icloud.com>
2026-08-12 16:03:22 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,137 @@
+// =====================================
+// FINEXA.exchange
+// WhatsApp Exchange Request System
+// =====================================
+
+
+// FINEXA WhatsApp Number
+const whatsappNumber = "526732304964";
+
+
+
+
+// Button Event
+
+document
+.getElementById("exchangeButton")
+.addEventListener("click", function(){
+
+
+
+    // Get values
+
+    const fromAsset =
+    document.getElementById("fromAsset").value;
+
+
+
+    const paymentMethod =
+    document.getElementById("paymentMethod").value;
+
+
+
+    const amount =
+    document.getElementById("amount").value;
+
+
+
+    const toAsset =
+    document.getElementById("toAsset").value;
+
+
+
+    const country =
+    document.getElementById("country").value;
+
+
+
+
+
+
+    // Validation
+
+
+    if(
+        fromAsset === "" ||
+        paymentMethod === "" ||
+        amount === "" ||
+        toAsset === "" ||
+        country === ""
+    ){
+
+
+        alert(
+        "Please complete all fields before requesting an exchange."
+        );
+
+
+        return;
+
+
+    }
+
+
+
+
+
+
+
+    // Create WhatsApp Message
+
+
+    const message =
+
+`Hello FINEXA,
+
+I would like to request an exchange.
+
+FROM:
+${amount} ${fromAsset}
+
+Payment Method:
+${paymentMethod}
+
+TO:
+${toAsset}
+
+Location:
+${country}
+
+
+Please send me your current exchange offer and further instructions.
+
+Thank you.`;
+
+
+
+
+
+
+
+    // Create WhatsApp URL
+
+
+    const whatsappURL =
+
+    "https://wa.me/"
+    + whatsappNumber
+    + "?text="
+    + encodeURIComponent(message);
+
+
+
+
+
+
+
+    // Open WhatsApp
+
+
+    window.open(
+        whatsappURL,
+        "_blank"
+    );
+
+
+
+});
